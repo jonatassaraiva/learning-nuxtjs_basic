9a2c3e5982b1da7ba5806afef2148d8b968cbeed
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2019-06-27 22:29:16 -0300

    Create layout of error

---
+++ layouts/error.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="container">
+    <h3> An error occurred, we're sorry! </h3>
+  </section>
+</template>
+
+<style>
+.container
+{
+  min-height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
