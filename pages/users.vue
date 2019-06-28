5bf6d1eb8ce85c50952e279e00b485ef187af01f
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2019-06-27 22:51:53 -0300

    Add users

---
+++ pages/users.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <input type="text" v-model="userId">
+    <button @click="onLoadUser">Load User</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userId: ''
+    };
+  },
+  methods: {
+    onLoadUser() {
+      this.$router.push('/users/' + this.userId);
+    }
+  },
+  layout: 'users'
+};
+</script>
+
+<style scoped>
+.container
+{
+  min-height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
+
